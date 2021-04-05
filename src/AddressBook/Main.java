18635c0eec2233efc510c4c93e1429cdbf21ce43
dangquocdatktmm <70972992+dangquocdatktmm@users.noreply.github.com>
2021-04-05 10:35:46 +0700

    thi

---
+++ src/AddressBook/Main.java
@@ -0,0 +1,57 @@
+package AddressBook;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HashMap<String, Contact> contacts = new HashMap<>();
+
+
+		while(true) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Input the application option:");
+		System.out.println("1. Add new contact");
+		System.out.println("2. Find a contact by name");
+		System.out.println("3. Display contacts");
+		System.out.println("4. Exit");
+
+		int option = scanner.nextInt();
+		
+			if(1 == option) {
+
+				System.out.println("Input name: ");
+				String name = scanner.next();
+				System.out.println("Input phone number: ");
+				String phoneNumber = scanner.next();
+				contacts.put(name, new Contact(name, phoneNumber));
+			} else if(2 == option) {
+
+				System.out.println("Input name to find: ");
+				String findName = scanner.next();
+				if(!contacts.containsKey(findName)) {
+					System.out.println("Not found");
+				}else {
+					System.out.println("Phone number of " + findName + " is: ");
+					System.out.println(contacts.get(findName).getPhone());
+				}
+			} else if(3 == option) {
+				if(!contacts.isEmpty()) {
+					System.out.println("Show all contacts");
+					contacts.entrySet().forEach(entry -> {
+						System.out.println("Name: " + entry.getValue().getName() +", phone number: "+ entry.getValue().getPhone());
+					});
+				}else {
+					System.out.println("No contacts");
+				}
+			} else if(4 == option) {
+				scanner.close();
+				System.exit(0);
+			}
+		}
+	}
+
+}
